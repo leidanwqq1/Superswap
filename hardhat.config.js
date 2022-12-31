4680756642c1f5bf5a37fc685f8fc0ae388643ab
DanLei <leidanwqq@gmail.com>
2022-12-31 11:17:01 +0800

    [Bugfix]: fix test-and-deploy.yml.

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    },
-    goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    }
-  }
+  // networks: {
+  //   localhost: {
+  //     url: "http://127.0.0.1:8545"
+  //   },
+  //   goerli: {
+  //     url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  //     accounts: [`0x${process.env.PRIVATE_KEY}`]
+  //   }
+  // }
 };
